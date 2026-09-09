d40226342b0cb56fd56cd1a84f09898421fa3744
Johannes Tallberg <johannes.tallberg@hotmail.com>
2026-09-09 09:38:50 +0200

    Added contition to while loop checking for number range

--- chessboard/Program.cs
+++ chessboard/Program.cs
@@ -4,7 +4,7 @@ int boardSize;
 
 
 
-while(!(int.TryParse(Console.ReadLine(), out boardSize)))
+while(!(int.TryParse(Console.ReadLine(), out boardSize)) || !(boardSize >= 3 && boardSize <= 50))
 {
     Console.WriteLine("Type a number between 3 - 50");
 }
