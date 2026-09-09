063dc7a9dc516063f179e71751ab0ca04e23a373
Valentin <119764111+valentincorceac@users.noreply.github.com>
2026-09-09 09:29:09 +0200

    added a while loop to test for right input

--- chessboard/Program.cs
+++ chessboard/Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("Welcome to chess board\nHow large do you want the board to be?\n(Answer with a number between 3 - 50): ");
+
+int boardSize;
+
+
+
+while(!(int.TryParse(Console.ReadLine(), out boardSize)))
+{
+    Console.WriteLine("Type a number between 3 - 50");
+}
