45f488973483fb854f7efd4d2e2c0a255b9e211e
Valentin <119764111+valentincorceac@users.noreply.github.com>
2026-09-09 10:11:42 +0200

    Render basic chessboard pattern

--- chessboard/Program.cs
+++ chessboard/Program.cs
@@ -8,3 +8,28 @@ while(!(int.TryParse(Console.ReadLine(), out boardSize)) || !(boardSize >= 3 && 
 {
     Console.WriteLine("Type a number between 3 - 50");
 }
+
+
+for(int i = 1; i <= boardSize; i++)
+{
+    for(int j = 1; j <= boardSize; j++)
+    {
+        if((j % 2 == 0) && (i % 2 == 0))
+        {
+            Console.Write("X ");
+        }
+        else if ((j % 2 == 0) && !(i % 2 == 0))
+        {
+            Console.Write("0 ");
+        }else if(!(j % 2 == 0) && !(i %2 == 0))
+        {
+            Console.Write("X ");
+        }
+        else
+        {
+            Console.Write("0 ");
+        }
+    }
+    Console.WriteLine();
+
+}
