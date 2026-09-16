0c5fa25ba31e72206de38d9211da83a7f708a7d4
Valentin <119764111+valentincorceac@users.noreply.github.com>
2026-09-16 08:20:53 +0200

    Created a GetSize method in the RenderBoard class

--- chessboard/Program.cs
+++ chessboard/Program.cs
@@ -1,13 +1,12 @@
-﻿Console.WriteLine("Welcome to chess board\nHow large do you want the board to be?\n(Answer with a number between 3 - 50): ");
+﻿using Chessboard;
+
+Console.WriteLine("Welcome to chess board\nHow large do you want the board to be?\n(Answer with a number between 3 - 50): ");
 
 int boardSize;
 
 
 
-while(!(int.TryParse(Console.ReadLine(), out boardSize)) || !(boardSize >= 3 && boardSize <= 50))
-{
-    Console.WriteLine("Type a number between 3 - 50");
-}
+RenderBoard.GetSize(out boardSize);
 
 
 for(int i = 1; i <= boardSize; i++)
---
+++ chessboard/RenderBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chessboard
+{
+    public class RenderBoard
+    {
+
+        public static void GetSize(out int boardSize)
+        {
+            while (!(int.TryParse(Console.ReadLine(), out boardSize)) || !(boardSize >= 3 && boardSize <= 50))
+            {
+                Console.WriteLine("Type a number between 3 - 50");
+            }
+        }
+
+
+    }
+}
