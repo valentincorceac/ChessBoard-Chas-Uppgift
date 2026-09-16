a979f2a33acfa74bd292b2ddf708c19cb133843a
Johannes Tallberg <johannes.tallberg@hotmail.com>
2026-09-16 08:33:29 +0200

    Moved while-loop to a Draw method in RenderBoard class attemt 2

--- chessboard/Program.cs
+++ chessboard/Program.cs
@@ -8,20 +8,4 @@ int boardSize;
 
 RenderBoard.GetSize(out boardSize);
 
-
-for(int i = 1; i <= boardSize; i++)
-{
-    for(int j = 1; j <= boardSize; j++)
-    {
-        if ((i + j) % 2 == 0 )
-        {
-            Console.Write("◻︎ ");
-        }
-        else
-        {
-            Console.Write("◼︎ ");
-        }
-    }
-    Console.WriteLine();
-
-}
+RenderBoard.Draw(boardSize);
--- chessboard/RenderBoard.cs
+++ chessboard/RenderBoard.cs
@@ -15,6 +15,25 @@ namespace Chessboard
             }
         }
 
+        public static void Draw(int boardSize)
+        {
+            for (int i = 1; i <= boardSize; i++)
+            {
+                for (int j = 1; j <= boardSize; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("◻︎ ");
+                    }
+                    else
+                    {
+                        Console.Write("◼︎ ");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+        }
 
     }
 }
